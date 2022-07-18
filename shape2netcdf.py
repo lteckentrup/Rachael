e9af520ce6f82d5b72a5cb5629e1fb97c780e0a6
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2022-07-18 09:25:41 +0200

    Update shape2netcdf.py

--- shape2netcdf.py
+++ shape2netcdf.py
@@ -8,16 +8,28 @@ from datetime import date
 
 date_created = date.today()
 
+### Read in shape file
 countries = gpd.read_file('countries.shp')
-ds = xr.open_mfdataset('../CRUJRA/pre/crujra.v2.0.5d.pre.1901.365d.noc.nc',
+
+### Read in netCDF with target grid
+ds = xr.open_mfdataset('../../CRUJRA/pre/crujra.v2.0.5d.pre.1901.365d.noc.nc',
                        chunks = {'time': 10})
 ds = ds.assign_coords(longitude=(((ds.lon + 180) % 360) - 180)).sortby('lon')
 
+### Grab country names
 names=list(countries.ctryName)
+
+### Assign numbers to country
 numbers=list(range(0,177))
+
+### Create legend (goes into attributes)
 attributes_prel=[str(n)+' = '+m for m,n in zip(names,numbers)]
 attributes=', '.join(attributes_prel)
 
+country_names_prel=[str(n)+' = '+m for m,n in zip(names,numbers)]
+country_names=', '.join(country_names_prel)
+
+### Convert shape xarray dataset
 countries_mask_poly=regionmask.Regions_cls(name='ctryName',
                                            numbers=list(range(0,177)),
                                            names=list(countries.ctryName),
@@ -27,6 +39,7 @@ countries_mask_poly=regionmask.Regions_cls(name='ctryName',
 
 mask = countries_mask_poly.mask(ds.isel(time=0), lat_name='lat', lon_name='lon')
 
+### Set attributes
 mask['lat'].attrs={'units':'degrees_north',
                    'long_name':'latitude',
                    'standard_name':'latitude',
@@ -39,9 +52,18 @@ mask['lon'].attrs={'units':'degrees_east',
 mask.attrs={'Countries':attributes,
             'Date_Created':str(date_created)}
 
+### write to netCDF
 mask.to_netcdf('countries_mask.nc',
                encoding={'lat':{'dtype': 'double'},
                          'lon':{'dtype': 'double'},
                          'region':{'dtype': 'double'}
                          }
                )
+
+### Write index, country names and code into external csv
+df = pd.DataFrame()
+df['Index']=numbers
+df['ctryCode']=gdf['ctryCode']
+df['ctryName']=gdf['ctryName']
+
+df.to_csv('country_names.csv',index=False)
