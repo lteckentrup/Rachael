11567a41808f0beb673b6bba19a4cbf63846d579
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2022-07-18 10:34:32 +0200

    Update country_average.py

--- country_average.py
+++ country_average.py
@@ -2,9 +2,11 @@ import xarray as xr
 import pandas as pd
 import numpy as np
 
+GCMs=['ACCESS-CM2', 'INM-CM4-8', 'CanESM5']
 ds_countries = xr.open_dataset('../country_shape/countries_mask_onedgree.nc')
 ds_gridarea = xr.open_dataset('../country_shape/gridarea.nc')
 
+### Calculate area weighted average for region
 def get_country_avg(var,stats,scen,GCM,period,region):
     ds = xr.open_dataset('../CMIP6/'+var+'_'+stats+'_'+scen+'/'+GCM+'/'+var+'_'+
                          GCM+'_'+scen+'_r1i1p1f1_gn_18500101-21001231.nc')
@@ -16,9 +18,25 @@ def get_country_avg(var,stats,scen,GCM,period,region):
 
     return(ds_region_mean.values.tolist())
 
-list_values = []
+### Get averages for all regions
+def all_countries(var,stats,scen,GCM,period):
+    list_values = []
+    region_indices=np.arange(0,177)
 
-region_indices=np.arange(0,177)
+    for r in region_indices:
+        list_values.append(get_country_avg(var,stats,scen,GCM,period,r))
 
-for r in region_indices:
-    list_values.append(get_country_avg(var,stats,scen,GCM,period,r))
+    return(list_values)
+
+### Read in file with country code and indices
+df = pd.read_csv('../country_shape/country_names.csv')
+
+### Write weighted averages to csv
+def write_to_csv(var,stats,scen,period):
+    for gcm in GCMs:
+        df[gcm] = all_countries(var,stats,scen,gcm,period)
+
+    df.to_csv(var+'_'+stats+'_'+scen+'_'+period+'.csv',index=False)
+
+### Call function
+write_to_csv('pr','COV','ssp245','')
